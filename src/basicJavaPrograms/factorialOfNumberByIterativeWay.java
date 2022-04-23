ed022733e0eb945fc4eb0e31f2664fc6bfd3155d
saharshshukla86@gmail.com <saharshshukla86@gmail.com>
2022-04-23 12:16:04 +0530

    updated

--- src/basicJavaPrograms/factorialOfNumberByIterativeWay.java
+++ src/basicJavaPrograms/factorialOfNumberByIterativeWay.java
@@ -11,7 +11,7 @@ public class factorialOfNumberByIterativeWay {
     }
     static int fact(int num){
         int product = 1;
-        for( int i = 1; i <= num; i++){
+        for( int i = num; i >= 1; i--){//int i = 1; i <= num; i++
             product = product * i;
         }
         return product;
